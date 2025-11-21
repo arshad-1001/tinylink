7d2f97445df64ecd444956b203515a71ad329b98
Arshad Shaik <shaikkarshad35@gmail.com>
2025-11-22 02:19:17 +0530

    Fix PrismaClient import for Prisma 7

--- lib/prisma.ts
+++ lib/prisma.ts
@@ -1,9 +1,9 @@
-import { PrismaClient } from "@prisma/client";
+import { PrismaClient } from "../lib/generated/prisma"; // New Prisma 7 output path
 
 const globalForPrisma = global as unknown as { prisma: PrismaClient };
 
 export const prisma =
-  globalForPrisma.prisma ||
+  globalForPrisma.prisma ??
   new PrismaClient({
     log: ["query", "error", "warn"],
   });
